e596d7deef9a3419c41c0ac80884911caa7cf32c
Daniel Arnold <darnold8@gmail.com>
2014-01-09 18:22:08 -0500

    override callback steps

--- lib/omniauth/strategies/hubspot.rb
+++ lib/omniauth/strategies/hubspot.rb
@@ -13,6 +13,36 @@ module OmniAuth
         :authorize_url => '/auth/authenticate'
       }
 
+      def callback_phase
+        raise OmniAuth::NoSessionError.new("Session Expired") if session['oauth'].nil?
+
+        opts = {}
+        if session['oauth'][name.to_s]['callback_confirmed']
+          opts[:oauth_verifier] = request['oauth_verifier']
+        else
+          opts[:oauth_callback] = callback_url
+        end
+
+        @access_token = params[:access_token]
+        @refresh_token = params[:refresh_token]
+        @expires_in = params[:expires_in]
+        super
+      rescue ::Timeout::Error => e
+        fail!(:timeout, e)
+      rescue ::Net::HTTPFatalError, ::OpenSSL::SSL::SSLError => e
+        fail!(:service_unavailable, e)
+      rescue ::OAuth::Unauthorized => e
+        fail!(:invalid_credentials, e)
+      rescue ::MultiJson::DecodeError => e
+        fail!(:invalid_response, e)
+      rescue ::OmniAuth::NoSessionError => e
+        fail!(:session_expired, e)
+      end
+
+      credentials do
+        {'access_token' => @access_token, 'refresh_token' => @refresh_token, 'expires_in' => @expires_in}
+      end
+
     end
   end
 end
