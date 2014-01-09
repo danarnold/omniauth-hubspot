448bb3985b004e37d67a92e1bd75f7ed41ac5652
Daniel Arnold <darnold8@gmail.com>
2014-01-09 18:34:52 -0500

    rolled back to oauth2, now I redid callback and credentials for that

--- lib/omniauth/strategies/hubspot.rb
+++ lib/omniauth/strategies/hubspot.rb
@@ -11,11 +11,41 @@ module OmniAuth
 
       option :client_options, {
         :site => 'https://app.hubspot.com',
-        :authorize_url => '/auth/authenticate',
-        :token_url => '/auth/authenticate'
+        :authorize_url => '/auth/authenticate'
       }
 
       option :authorize_options, [:scope, :portalId]
+
+      attr_accessor :refresh_token
+      attr_accessor :expires_in
+
+      def callback_phase
+        if request.params['error'] || request.params['error_reason']
+          raise CallbackError.new(request.params['error'], request.params['error_description'] || request.params['error_reason'], request.params['error_uri'])
+        end
+        if !options.provider_ignores_state && (request.params['state'].to_s.empty? || request.params['state'] != session.delete('omniauth.state'))
+          raise CallbackError.new(nil, :csrf_detected)
+        end
+
+        self.access_token = params[:access_token]
+
+        super
+      rescue ::OAuth2::Error, CallbackError => e
+        fail!(:invalid_credentials, e)
+      rescue ::MultiJson::DecodeError => e
+        fail!(:invalid_response, e)
+      rescue ::Timeout::Error, ::Errno::ETIMEDOUT, Faraday::Error::TimeoutError => e
+        fail!(:timeout, e)
+      rescue ::SocketError, Faraday::Error::ConnectionFailed => e
+        fail!(:failed_to_connect, e)
+      end
+
+      credentials do
+        hash = {'token' => access_token}
+        hash.merge!('refresh_token' => refresh_token)
+        hash.merge!('expires_in' => expires_in)
+        hash
+      end
 
     end
   end
